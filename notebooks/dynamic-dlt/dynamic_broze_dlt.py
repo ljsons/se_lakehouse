93fdfad847ed21b0c62d401094ee74c602a4abac
ljsons <ljsons>
2024-05-17 18:55:59 +0000

    Wrapped up dynamic bronze loading

--- notebooks/dynamic-dlt/dynamic_broze_dlt.py
+++ notebooks/dynamic-dlt/dynamic_broze_dlt.py
@@ -1,6 +1,14 @@
 # Databricks notebook source
-#import dlt
+import dlt
 import json
+from pyspark.sql.functions import *
+from datetime import datetime
+
+
+# COMMAND ----------
+
+#get this from Key Vault 
+dataLakeAcct = "abfss://datalake@lsyckdemodatalake.dfs.core.windows.net"
 
 # COMMAND ----------
 
@@ -8,44 +16,71 @@ dbutils.widgets.text("paramsJson", "")
 
 # COMMAND ----------
 
-testParams = """{
-    "source": "pos",
-    "tableName": "order_coupon_dom",
-    "landingZonePath": "edw/files/input/pos/extract_type/OC2/",
+
+
+# COMMAND ----------
+
+testParams = """[{
+    "source": "bikeshare",
+    "tableName": "divvystations",
+    "landingZonePath": "/divvy/",
     "format": "csv",
     "includesHeader": "true",
-    "encoding": "UTF-16",
-    "delimiter": "t",
+    "encoding": "UTF-8",
+    "delimiter": ",",
     "bronzeTableSettings": {
-        "name": "order_coupon_dom"
+        "name": "divvystations"
     }
-}"""
-
-# COMMAND ----------
+}]"""
 
 passedParams =  dbutils.widgets.get("paramsJson") 
-params = json.loads(passedParams or testParams)
-
-print(params.get("source"))
-
-print(f"bronze_{params.get('source')}_{params.get('tableName')}")
-
-print(f"_{params.get('tableName')}")
+datasets = json.loads(passedParams or testParams)
 
 # COMMAND ----------
 
-def generate_bronze_table(params):
-    tableProperties = {"quality": "bronze"}
-    
-    additionalTableProperties = params.get('BronzeSettings', {}).get('tableProperties')
+def load_bronze_table(params):
+    tableName = params.get('tableName')
+    schemaName = params.get('schemaName')
+    dataPath = f"{dataLakeAcct}{params.get('landingZonePath')}"
+    primaryKeys = params.get("primaryKeys")
 
-    if additionalTableProperties: 
-        tableProperties.update(additionalTableProperties)
-    
+    cloudFile = {
+        "cloudFiles.format": f"{params.get('format')}",
+        "delimiter": f"{params.get('delimiter')}",
+        "header": f"{params.get('includesHeader')}",
+        "encoding": f"{params.get('encoding')}",
+        "cloudFiles.inferColumnTypes": "true",
+        "cloudFiles.schemaHints": "online_date string"
+    }
+
+    driftConf = {
+        "cloudFiles.schemaEvolutionMode": "rescue",
+        "rescuedDataColumn": "_rescued_data"
+    }
+
     @dlt.table(
-        name = f"
+        name = f"bronze_{schemaName}_{tableName}"
     )
+    def incremental_bronze():
+        df = (spark.readStream
+                .format("cloudFiles")
+                .options(**cloudFile)
+                .options(**driftConf)
+                .load(dataPath)
+        )
+        
+        ## Currenlty not working on first run.
+        ##df = df.withColumn("sourceFile", col(source_metadata.file_path) )
+        df = df.withColumn("_load_date", lit(datetime.now()))
+        return (
+            df
+        )
 
 # COMMAND ----------
 
-""
+for dataset in datasets:
+    load_bronze_table(dataset)
+
+# COMMAND ----------
+
+
--- notebooks/process_se_data.py
+++ notebooks/process_se_data.py
@@ -1,2 +1,52 @@
 # Databricks notebook source
+import dlt
+import json
+from pyspark.sql.functions import *
+from datetime import datetime
+
+# COMMAND ----------
+
+dbutils.widgets.text("paramsJson", "")
+
+# COMMAND ----------
+
+testParams = """[
+    {
+        "schemaName": "pos",
+        "tableName": "order_coupon_dom",
+        "landingZonePath": "/edw/files/input/pos/extract_type/OC2/",
+        "format": "csv",
+        "includesHeader": "true",
+        "encoding": "UTF-16",
+        "delimiter": "\\t",
+        "bronzeTableSettings": {
+            "name": "order_coupon_dom"
+        },
+        "primaryKeys" : ["OrdCpnNbr"]
+    },
+    {
+        "schemaName": "pos",
+        "tableName": "order_coupon_dom",
+        "landingZonePath": "/edw/files/input/pos/extract_type/OCX/",
+        "format": "csv",
+        "includesHeader": "true",
+        "encoding": "UTF-16",
+        "delimiter": "\\t",
+        "bronzeTableSettings": {
+            "name": "order_coupon_intl"
+        },
+        "primaryKeys" : ["OrdCpnNbr"]
+    }
+]"""
+
+passedParams =  dbutils.widgets.get("paramsJson") 
+datasets = json.loads(passedParams or testParams)
+
+# COMMAND ----------
+
+for dataset in datasets:
+   
+
+# COMMAND ----------
+
 
